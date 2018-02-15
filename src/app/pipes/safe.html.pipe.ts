c8aedda7c645f60e7db1817f068d404cba635ac1
David Martinez Ros <programador2.informatica@upf.edu>
2018-02-15 10:17:04 +0100

    skip the innerhtml for loading texts to experience page, because is not necessary, but still doesn't work when change the language

--- src/app/pipes/safe.html.pipe.ts
+++ src/app/pipes/safe.html.pipe.ts
@@ -6,7 +6,7 @@ export class SafeHtmlPipe implements PipeTransform {
   
   constructor(private sanitizer: DomSanitizer) {}
 
-  transform(html) {
+  transform(html: string) {
     return this.sanitizer.bypassSecurityTrustHtml(html);
   }
 } 
