b0e732aeddf5971200396ff878b478f51b7a5b6e
David Martinez Ros <davidnezan@gmail.com>
2018-05-03 21:16:50 +0200

    modify server.ts for load https using the server configuration and trying to load all the rest of urls that now some loads and some no

--- server.ts
+++ server.ts
@@ -20,6 +20,12 @@ enableProdMode();
 const app = express();
 
 const PORT = process.env.PORT || 4000;
+const HTTPS_PORT = process.env.HTTPS_PORT || 4443;
+
+const KEY_CERTIFICATE = process.env.KEY_CERTIFICATE;
+const CRT_CERTIFICATE = process.env.CRT_CERTIFICATE;
+const PASSWORD_CERTIFICATE = process.env.PASSWORD_CERTIFICATE;
+
 const DIST_FOLDER = join(process.cwd(), 'dist');
 
 // Our index.html we'll use as our template
@@ -50,12 +56,66 @@ app.set('views', join(DIST_FOLDER, 'browser'));
 // Server static files from /browser
 app.get('*.*', express.static(join(DIST_FOLDER, 'browser')));
 
+// Our page routes
+export const routes: string[] = [
+  'main',
+  'dashboard'
+];
+
 // All regular routes use the Universal engine
+app.get('/', (req, res) => {
+  res.render(join(DIST_FOLDER, 'browser', 'index.html'), { req });
+});
+routes.forEach(route => {
+  app.get(`/${route}`, (req, res) => {
+    res.render(join(DIST_FOLDER, 'browser', 'index.html'), { req });
+  });
+  // Route pattern
+  app.get(`/${route}/*`, (req, res) => {
+    res.render(join(DIST_FOLDER, 'browser', 'index.html'), { req });
+  });
+});
+
+app.get('*', function(req, res) {
+  res.render(join(DIST_FOLDER, 'browser', req.url), { req });
+});
+/*
+app.get('*', function(req, res) {
+  res.render('http://localhost:' + PORT + '/' + req.url);
+});
+*/
+// All regular routes use the Universal engine
+/*
 app.get('*', (req, res) => {
     res.render(join(DIST_FOLDER, 'browser', 'index.html'), { req });
 });
+*/
 
-// Start up the Node server
-app.listen(PORT, () => {
+var http = require('http');
+
+var httpServer = http.createServer(app);
+
+// Start up the Node server at PORT
+httpServer.listen(PORT, () => {
   console.log(`Node server listening on http://localhost:${PORT}`);
 });
+
+var fs = require('fs');
+var https = require('https');
+if(KEY_CERTIFICATE && CRT_CERTIFICATE && PASSWORD_CERTIFICATE) {
+
+  var privateKey  = fs.readFileSync(KEY_CERTIFICATE, 'utf8');
+  var certificate = fs.readFileSync(CRT_CERTIFICATE, 'utf8');
+
+  var credentials = {
+    key: privateKey,
+    cert: certificate,
+    passphrase: PASSWORD_CERTIFICATE
+  };
+  var httpsServer = https.createServer(credentials, app);
+
+  // Start up the Node server at HTTP_PORT
+  httpsServer.listen(HTTPS_PORT, () => {
+    console.log(`Node server listening on http://localhost:${HTTPS_PORT}`);
+  });
+}
