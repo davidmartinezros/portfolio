0121d591d7cd7b471e8030b57ca113fe9c632f31
David Martinez Ros <davidnezan@gmail.com>
2019-04-26 19:03:42 +0200

    resolve some deploy to prod errors

--- src/app/firebase-auth/auth.service.ts
+++ src/app/firebase-auth/auth.service.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 import { Router } from "@angular/router";
 import { AngularFireAuth } from 'angularfire2/auth';
 import { AngularFireDatabase } from 'angularfire2/database';
-import * as firebase from 'firebase';
-
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
 
 @Injectable()
 export class AuthService {
