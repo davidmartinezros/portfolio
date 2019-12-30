e5bb86399d0b8c213a4af872942b50a0a3fc9419
David Martinez Ros <davidnezan@gmail.com>
2019-12-30 17:49:24 +0100

    resolve some scroll and javascript issues

--- src/assets/scripts/navigate.js
+++ src/assets/scripts/navigate.js
@@ -8,7 +8,7 @@
         event.preventDefault();
         var hash = this.hash;
         $('html, body').animate({
-          scrollTop: $(hash).offset().top
+          scrollTop: $(hash).offset().top -30
         }, 400, function () {
           window.location.hash = hash;
         });
